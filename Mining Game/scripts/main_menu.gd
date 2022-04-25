b38516de72484a250503ee38fe2b204042886288
SzymonPiskorz <c00242808@itcarlow.ie>
2022-04-25 18:41:55 +0100

    Added settings screen and background music

--- Mining Game/scripts/main_menu.gd
+++ Mining Game/scripts/main_menu.gd
@@ -10,7 +10,7 @@ func _on_Play_pressed():
 
 
 func _on_Settings_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene("res://scenes/settingScreen.tscn")
 
 
 func _on_Help_pressed():
