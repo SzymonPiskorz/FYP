b6133c4f0ed0f23770bd9af1444370a5fd78fc54
SzymonPiskorz <c00242808@itcarlow.ie>
2022-04-25 12:43:09 +0100

    Added more improvements and finished it

--- Mining Game/scripts/HUD.gd
+++ Mining Game/scripts/HUD.gd
@@ -8,6 +8,9 @@ onready var ItemSlot1_label = $Footer/ItemSlot1/Label
 onready var ItemSlot2_tex = $Footer/ItemSlot2/ItemTexture
 onready var ItemSlot2_label = $Footer/ItemSlot2/Label
 
+var time : float
+var TIME_PERIOD : float  = 20
+
 func _ready():
 	ItemSlot1_label.text = 1 as String
 	ItemSlot1_tex.texture = load("res://assets/weaponsAndUtilities/eternalGaze.png")
@@ -22,4 +25,10 @@ func _on_Player_tokens_changed(value) -> void:
 	TokensCollected.text = "Tokens Collected: " + value as String
 
 func _process(_delta):
+	time += _delta
+	
+	if time > TIME_PERIOD:
+		time = 0
+		EnjinApi.get_token_amount()
+	
 	OverallTokens.text = "Overall Tokens: " + Data.overallTokens as String
