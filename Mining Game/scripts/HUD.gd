c0f69c437ac098528fe426424c738cf88ace5600
SzymonPiskorz <c00242808@itcarlow.ie>
2022-04-25 14:28:35 +0100

    Finished shop screen and added two more items

--- Mining Game/scripts/HUD.gd
+++ Mining Game/scripts/HUD.gd
@@ -7,9 +7,15 @@ onready var ItemSlot1_tex = $Footer/ItemSlot1/ItemTexture
 onready var ItemSlot1_label = $Footer/ItemSlot1/Label
 onready var ItemSlot2_tex = $Footer/ItemSlot2/ItemTexture
 onready var ItemSlot2_label = $Footer/ItemSlot2/Label
+onready var ItemSlot3_tex = $Footer/ItemSlot3/ItemTexture
+onready var ItemSlot3_label = $Footer/ItemSlot3/Label
+onready var ItemSlot3_amount = $Footer/ItemSlot3/NumAmount
+onready var ItemSlot4_tex = $Footer/ItemSlot4/ItemTexture
+onready var ItemSlot4_label = $Footer/ItemSlot4/Label
+onready var ItemSlot4_amount = $Footer/ItemSlot4/NumAmount
 
 var time : float
-var TIME_PERIOD : float  = 20
+var TIME_PERIOD : float  = 15
 
 func _ready():
 	ItemSlot1_label.text = 1 as String
@@ -17,6 +23,12 @@ func _ready():
 	
 	ItemSlot2_label.text = 2 as String
 	ItemSlot2_tex.texture = load("res://assets/weaponsAndUtilities/Pick1.png")
+	
+	ItemSlot3_label.text = 3 as String
+	ItemSlot3_tex.texture = load("res://assets/consumables/smallHpBottle1.png")
+	
+	ItemSlot4_label.text = 4 as String
+	ItemSlot4_tex.texture = load("res://assets/consumables/HpBottle1.png")
 
 func _on_Player_health_changed(value : int) -> void:
 	HealthBar.value = value
@@ -32,3 +44,5 @@ func _process(_delta):
 		EnjinApi.get_token_amount()
 	
 	OverallTokens.text = "Overall Tokens: " + Data.overallTokens as String
+	ItemSlot3_amount.text = Data.small_hp_bottle_amount as String
+	ItemSlot4_amount.text = Data.hp_bottle_amount as String
